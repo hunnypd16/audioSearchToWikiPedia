4243ab9e50f7f801d6126c2be964432f7202c814
hunny <hnyd016@gmail.com>
2022-02-28 15:01:08 +0530

    updated code

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-print('hi')
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import time
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager w
+from selenium.webdriver.common.by import By
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get('https://www.google.co.in/')
+driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div[1]/div/div[2]/input').send_keys('wikipedia')
+driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/form/div[1]/div[1]/div[2]/div[2]/div[5]/center/input[1]').click()
+driver.find_element(By.XPATH,'/html/body/div[7]/div/div[10]/div/div[2]/div[2]/div/div/div[2]/div/div/div[1]/div/div/div[1]/div/a/h3').click()
+driver.find_element(By.XPATH,'/html/body/div[3]/form/fieldset/div/input').send_keys('what is the size of sun')
+driver.find_element(By.XPATH,'/html/body/div[3]/form/fieldset/button/i').click()
+driver.find_element(By.XPATH, '/html/body/div[3]/div[3]/div[4]/div[3]/ul/li[1]/div[1]/a').click()
+wiki_text = driver.find_element(By.XPATH,'/html/body/div[3]/div[3]/div[5]/div[1]/p[2]').text
+print(wiki_text)
+#sudo apt install espeak
+import pyttsx3
+engine = pyttsx3.init()
+engine.say(wiki_text)
+engine.runAndWait()
